4ad8b12e35f179687da07bb218b9c5fca8655a15
Weijun Li <me@liweijun.com>
2026-01-31 07:19:24 -0800

    feat(core): add user/content DTOs

---
+++ apps/core/internal/service/dto_test.go
@@ -0,0 +1,12 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/RevieU-Corp/revieu-backend/apps/core/internal/dto"
+)
+
+func TestDTOJSONTags(t *testing.T) {
+	_ = dto.PublicProfileResponse{}
+	_ = dto.PostItem{}
+}
